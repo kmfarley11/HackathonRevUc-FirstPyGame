7c010a6c678e01d40f4db58556bd97e8defa8510
farleykm <farleykm@mail.uc.edu>
2015-05-17 02:24:54 -0400

    logic for multiple enemies and knockback

--- PythonGame/enemy.py
+++ PythonGame/enemy.py
@@ -7,19 +7,35 @@ class Enemy(pygame.sprite.Sprite):
         super().__init__()
         self.image = self.enemy1
         self.rect = self.image.get_rect()
+        '''
         self.rect.x = 750
         self.rect.y = 320
+        '''
         self.side = 0
         self.speed = 15
         self.it = 0
+        #self.hidden = False
         self.health = 100
-        self.canBeHit = True
 
     #TODO: maybe add rand arithmetic to sometimes run away
     def update(self,x,y):
         if(self.rect.right < x + 50) and not self.rect.colliderect(design.right): self.rect.x += self.speed
-        if(self.rect.bottom < y + 50) and not self.rect.colliderect(design.bottom): self.rect.y += self.speed
+        if(self.rect.bottom < y + 175) and not self.rect.colliderect(design.bottom): self.rect.y += self.speed
         if(self.rect.left > x + 65) and not self.rect.colliderect(design.left): self.rect.x -= self.speed
         if(self.rect.top > y - 40) and not self.rect.colliderect(design.top): self.rect.y -= self.speed
+
+    def update2(self,x,y,hidden):
+        # use delta values to set signs for proper bounce direction
+        if (x < 0) : x = -1
+        else : x = 1
+        if (y < 0) : y = -1
+        else : y = 1
+
+        if not self.rect.colliderect(design.right): self.rect.x += self.speed + x * 15
+        if not self.rect.colliderect(design.bottom): self.rect.y += self.speed + y * 15
+        if not self.rect.colliderect(design.left): self.rect.x -= self.speed - x * 15
+        if not self.rect.colliderect(design.top): self.rect.y -= self.speed - y * 15
+
+        if hidden : del self.enemy1
 
     enemy1 = pygame.image.load('resources/crazyShiz.png')
